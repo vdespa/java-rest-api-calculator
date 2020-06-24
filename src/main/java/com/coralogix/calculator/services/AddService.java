e8b98e7172c354ca9ef38082d43f2001da0bba42
Valentin Despa <valentin@vdespa.com>
2020-06-24 16:27:37 -0600

    Make test fail.

--- src/main/java/com/coralogix/calculator/services/AddService.java
+++ src/main/java/com/coralogix/calculator/services/AddService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddService {
     public Result add(int a, int b) {
-        int sum = a + b;
+        int sum = a - b;
         return new Result(sum);
     }
 }
